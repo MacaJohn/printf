711b82f446bc3a65ce3b8b9821d14609436f48ad
Quesses <quesses1@gmail.com>
2022-08-09 21:57:44 +0000

    Final testing

--- test/main2.c
+++ test/main2.c
@@ -13,6 +13,8 @@ int main(void)
 	int len2;
 	unsigned int ui;
 	void *addr;
+	char c = 'a';
+	char *str = "yea! yea!! yea!!!";
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
@@ -20,20 +22,20 @@ int main(void)
 	addr = (void *)0x7ffe637541f0;
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Lenght:[%d, %i]\n", len2, len2);
-	_printf("Negative:[%d]\n", -762534);
-	printf("Negative:[%d]\n", -762534);
+	_printf("Negative:[%d] and [%d]\n", -762534, -9999);
+	printf("Negative:[%d] and [%d]\n", -762534, - 9999);
 	_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
 	_printf("Unsigned octal:[%o]\n", ui);
 	printf("Unsigned octal:[%o]\n", ui);
 	_printf("Unsigned hexadecimal:[%x, %x]\n", ui, ui);
 	printf("unsigned hexadecimal:[%x, %x]\n", ui, ui);
-	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "String here");
-	printf("String:[%s]\n", "String here");
-	_printf("Address:[%p]\n", addr);
-	printf("Address:[%p]\n", addr);
+	_printf("Character:[%c] and [%c]\n", 'H', c);
+	printf("Character:[%c] and [%c]\n", 'H', c);
+	_printf("String:[%s] and [%s]\n", "String here", str);
+	printf("String:[%s] and [%s]\n", "String here", str);
+	_printf("Address:[%p] and [%p]\n", addr, &c);
+	printf("Address:[%p] and [%p]\n", addr, &c);
 	len = _printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
